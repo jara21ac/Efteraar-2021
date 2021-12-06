114f4056b5733f888b06cc2fa6d07fe94131ee0c
Jacob <jara21ac@student.cbs.dk>
2021-12-06 12:10:46 +0100

    Organising

--- server.js
+++ server.js
@@ -1,20 +1,38 @@
+//npm init for at vi kan bruge node_modules 
+
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
 //Imports
+    //npm i express. Express server, application server
+    //npm i ejs vores template sprog
+    //npm i --save-dev ("for development only") nodemon (genstarter serveren hver gang der gemmes)
+    //dotenv gør det muligt for os at bruge "environment variables" som vi kan lagre i en dotenv fil som vi derved loade ind i vores server.
+    //Vi tilføjer .env(da den kunne indholde sensitive oplysninger) og nodemodules til vores .gitignore fil
+
+
+    //Import Express
 const express = require('express')
+    //app variable
 const app = express()
+    //hashedPassowrd
 const bcrypt = require('bcrypt')
+    //Authenticate
 const passport = require('passport')
+    //
 const flash = require('express-flash')
+    //
 const session = require('express-session')
+    //Gør det muligt for os at bruge PUT og DELETE i vores EJS filer
 const methodOverride = require('method-override')
+    //Gør det muligt for os at gemme data i JSON filer
 const fs = require('fs')
 
 const initializePassport = require('./passport-config')
+const { request } = require('http')
 initializePassport(
-    passport, 
+    passport,
     email => users.find(user => user.email === email),
     id => users.find(user => user.id === id)
 )
@@ -24,11 +42,16 @@ initializePassport(
 
 const users = [];
 
+//PORT
+//Export, app.adress (error)
+module.exports = app.listen(3001), users
 
 //app.set/use
+
 app.set('views', './views')
+//Muliggøre EJS syntaks
 app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(session({
     secret: process.env.SESSION_SECRET,
@@ -40,16 +63,36 @@ app.use(passport.session())
 app.use(methodOverride('_method'))
 
 //PORT
-app.listen(3000)
+//Export, app.adress
+module.exports = app.listen(3000), users
+
+//Authentication
+function checkAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next()
+    }
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/')
+    }
+    next()
+}
 
 //layout
 
-    //localhost:3000
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name})
-})
+//localhost:3000
 
-    //localhost:3000/login
+app.get('/', checkAuthenticated, (req, res) => {
+    //"/" hjemmeside ruten
+    //Authentication funktionen bruges til at tjekke ???
+    //(req, res) request og response variable
+    //=> function sender dem til vores index
+    res.render('index.ejs', { name: req.user.name })
+}) //<form> i EJS for at definere vores inputs
+
+//localhost:3000/login
 app.get('/login', checkNotAuthenticated, (req, res) => {
     res.render('login.ejs')
 })
@@ -60,21 +103,27 @@ app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     failureFlash: true
 }))
 
-    //localhost:3000/ --> Logout
+//localhost:3000/ --> Logout
+//DELETE til at slette
 app.delete('/logout', (req, res) => {
+    //logout som vi kan bruger fra vores ???
     req.logout()
     res.redirect('/login')
 })
 
-    //localhost:3000/ --> delete
+//localhost:3000/ --> delete
 app.delete('/', (req, res) => {
+    //Vi sletter (splice) hos "users" den højre del (index 1) af objektet
     users.splice(0, users.length);
+    //Vi bruger fs til at gemme filer. De bliver skrevet hen i sen synkron form til vores "user.json" i "Profiles" mappen.
+    //Dertil bruger vi JSON.stringify til at gøre vores users fra objekt til stringes
+    //For at gøre det nemmere at se på bruger vi null, 2 så det står under hinanden i stedet for i forlængelse
     fs.writeFileSync('Profiles/user.json', JSON.stringify(users, null, 2));
     req.logOut()
     res.redirect('/login')
 })
 
-    //localhost:3000/register
+//localhost:3000/register
 
 app.get('/register', checkNotAuthenticated, (req, res) => {
     res.render('register.ejs')
@@ -83,6 +132,7 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
 app.post('/register', checkNotAuthenticated, async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        //hashedpassword for at kryptere adgangskoden
         users.push({
             id: Date.now().toString(),
             name: req.body.name,
@@ -90,13 +140,15 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
             password: hashedPassword
         })
         res.redirect('/login')
+        //Hvis det lykkes bliver vi ført hen til /login
         fs.writeFileSync('Profiles/user.json', JSON.stringify(users, null, 2));
     } catch {
+        //Hvis det fejler bliver vi ført hen til /register
         res.redirect('/register')
     }
 })
 
-    //localhost:3000/update
+//localhost:3000/update
 app.get('/update', checkAuthenticated, (req, res) => {
     res.render('update.ejs')
 })
@@ -115,24 +167,10 @@ app.put('/update', async (req, res) => {
         fs.writeFileSync('Profiles/user.json', JSON.stringify(users, null, 2));
     } catch {
         res.redirect('/update')
-    }   
+    }
     //console.log(users)
 })
 
-
-    //Authentication
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
-    }
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/')
-    }
-    next()
-}
 
 
 //Create Product
@@ -143,7 +181,7 @@ app.get('/product', checkAuthenticated, (req, res) => {
     res.render('product.ejs', {
         name: req.user.name,
         product: product
-    }) 
+    })
 })
 
 app.post('/', checkAuthenticated, (req, res) => {
@@ -156,7 +194,8 @@ app.post('/', checkAuthenticated, (req, res) => {
     })
     res.redirect('/product')
     fs.writeFileSync('Profiles/product.json', JSON.stringify(product, null, 2));
-    console.log(product)
+    //console.log(product)
+    //Testing
 })
 
 //Update Product
@@ -174,7 +213,7 @@ app.put("/updateProduct", async (req, res) => {
             price: req.body.price,
             image: await req.body.image
         })
-        product.splice(0,1);
+        product.splice(0, 1);
         res.redirect("/product")
         fs.writeFileSync('Profiles/product.json', JSON.stringify(product, null, 2));
     } catch {
@@ -192,7 +231,12 @@ app.delete("/product", (req, res) => {
 })
 
 //One Category
-app.get("/categoryProduct", (req, res) => {
-    res.render("categoryProduct.ejs")
+
+app.get("/product/:category", checkAuthenticated, (req, res) => {
+    const categories = product.find(c => c.category === req.params.category)
+    if (!categories) return "Wrong category"
+    res.render("categoryProduct.ejs", {categories: categories})
 })
 
+//CSS
+app.use(express.static(__dirname + '/public'));
